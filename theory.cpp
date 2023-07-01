ab251b671993da39af0da678f075157f0e821829
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-07-01 22:07:17 +0530

    Add files via upload

---
+++ theory.cpp
@@ -0,0 +1,5 @@
+// namespaces 
+//used bcz of the naming conflict . we define our own library and can define the things in it and can use it as
+//libname::operator
+//:: is callled as the scope resolution operator
+
