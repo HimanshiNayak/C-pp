01140033c4b161daf7b22a8bf24d75b2f9928082
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-07-06 23:32:53 +0530

    Update theory.cpp

--- theory.cpp
+++ theory.cpp
@@ -2,4 +2,4 @@
 //used bcz of the naming conflict . we define our own library and can define the things in it and can use it as
 //libname::operator
 //:: is callled as the scope resolution operator
-
+//cin>>f>>g>>h;   will take three variable values...
