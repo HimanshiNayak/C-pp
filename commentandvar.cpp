1187c419c9112f5b6fe0bea9ab06519113f5a042
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-07-08 23:58:11 +0530

    Add files via upload

---
+++ commentandvar.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//this is a s ingle line comment
+int main(){
+    int a = 6;
+    cout<< "Hello world"<< a;
+    return 0;
+}
+/*This is a multiline comment*/
+
+
